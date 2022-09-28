8cebd84dd78ade8eb8fa423ba7632a9c7ae6e5cd
LloydAssan <assanlloyd@gmail.com>
2022-09-28 20:46:37 +0000

    working with lists

---
+++ lab14.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+
+    input("Enter your name here: ")
+    name = input()
+
+    print("Your name is : ", name)
+
+    input("Enter day of the week too  ")
+    day = input()
+    
+    print (""Hello ", name, "!", "Happy ", day")
+main()
